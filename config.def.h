1d36fec9ab6c5426cce2eda51eb625a052519ed0
Agahnim <naguiagahnim@proton.me>
2026-08-19 17:27:20 +0200

    follow reuse specifications for other files

--- config.def.h
+++ config.def.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2026 Agahnim
+// SPDX-License-Identifier: GPL-3.0-only
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
--- types.h
+++ types.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2026 Agahnim
+// SPDX-License-Identifier: GPL-3.0-only
+
 typedef union {
 	const void *v;
 	int i;
