1dd70198c890042c49cdef1fd5a5103e8ae1788f
Agahnim <naguiagahnim@proton.me>
2026-08-19 17:04:32 +0200

    format code and add layershell support (closes #5)

--- config.def.h
+++ config.def.h
@@ -8,13 +8,13 @@
 static const int32_t gaps = 50;
 
 static const char *const autostart[] = {
-  "swaybg", "-i", "~/Images/Wallpapers/fiswordhd.png", NULL
+  "swaybg", "-i", "mywallpaper", NULL
 };
 
 // Use this to define your terminal command
 // All command vectors follow the same logic
-// static const char *termcmd[] = { "st-wl", "an-arg", "why-not-another-arg", NULL} ;
-static const char *termcmd[] = { "wezterm", NULL} ;
+// static const char *termcmd[] = { "st-wl", "an-arg", "why-not-another-arg", NULL } ;
+static const char *termcmd[] = { "wezterm", NULL } ;
 
 static const char *menucmd[] = { "bemenu-run", "-p", "Run:", "-l", "10", NULL };
 
